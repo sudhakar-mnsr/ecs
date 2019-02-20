7645d4771312c378c3d4782b5d116543944adc8c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-02-20 14:45:21 +0530

    added cleanup services

---
+++ cleanup_services.sh
@@ -0,0 +1,6 @@
+aws servicediscovery list-services --region us-east-2
+#get the id from above command
+aws sercicediscovery delete-service --id <service_discovery_service_id> --region us-east-2
+aws servicediscovery list-nmespaces --region us-east-2
+#get the id from above command
+aws servicediscovery delete-namespace --id <service_discovery_namespace_id> --region us-east-2
